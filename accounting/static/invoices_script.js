c8c3f6d2db71a7dc09938988e4ac742d34d098cf
Moises Terrones <moises.tersal@gmail.com>
2019-02-13 17:53:03 -0600

    Problem 10
    Added a basic implementation to view the Policies, this implementation only displays one policy at a time with the invoices for that policy, I think that this is te minimum that the problem requests.
    
    I still need to add improvements to validate the data received by the server and how it should be handled (I guess that this is part of problem 11).
    
    Finally, I think that it will be a good idea to add additional implementation to create or edit policies, I'm planning to add such implementation as part of the BONUS tasks.

--- accounting/static/invoices_script.js
+++ accounting/static/invoices_script.js
@@ -1,6 +1,51 @@
-function PolicyViewMode() {
+$(document).ready(function() {
+
+ko.bindingHandlers.fadeVisible = {
+    init: function(element, valueAccessor) {
+        // Start visible/invisible according to initial value
+        var shouldDisplay = valueAccessor();
+        $(element).toggle(shouldDisplay);
+    },
+    update: function(element, valueAccessor) {
+        // On update, fade in/out
+        var shouldDisplay = valueAccessor();
+        shouldDisplay ? $(element).fadeIn() : $(element).fadeOut();
+    }
+};
+function PolicyViewModel() {
     // Data
     var self = this;
-    self.date = ko.observable()
-    self.policy_id = ko.observable()
+    self.date = ko.observable();
+    self.policy_id = ko.observable();
+    self.amount_due = ko.observable(0);
+    self.invoices = ko.observableArray([]);
+
+    // Send current policy information to the server
+    self.consult = function() {
+        $.ajax({
+            url: "/consult_policy",
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: ko.toJSON({ date: self.date, policy_id: self.policy_id }),
+        }).done(function(response) {
+            self.amount_due(response.total_balance);
+            self.invoices([])
+            for(var i = 0; i < response.invoices.length; i++) {
+                invoice = {
+                    bill_date: response.invoices[i].bill_date,
+                    due_date: response.invoices[i].due_date,
+                    cancel_date: response.invoices[i].cancel_date,
+                    amount_due: response.invoices[i].amount_due,
+                }
+                self.invoices.push(invoice);
+            }
+        })
+    };
 }
+
+// Activate knockout.js
+ko.applyBindings(new PolicyViewModel());
+
+});
+
--- accounting/views.py
+++ accounting/views.py
@@ -1,14 +1,51 @@
+from datetime import date, datetime
+import json
 # You will probably need more methods from flask but this one is a good start.
-from flask import render_template
+from flask import render_template, request
 
 # Import things from Flask that we need.
 from accounting import app, db
 
 # Import our models
 from models import Contact, Invoice, Policy
+from utils import PolicyAccounting
 
 # Routing for the server.
 @app.route("/")
 def index():
     # You will need to serve something up here.
     return render_template('index.html')
+
+# Process data
+@app.route("/consult_policy", methods=['POST'])
+def consult_data():
+    response = {}
+    list_invoices = []
+    data = request.json
+    curr_date = datetime.strptime(data['date'], "%Y-%m-%d")
+    policy_id = int(data['policy_id'])
+    pa = PolicyAccounting(policy_id)
+
+    # Get current balance to date
+    response["total_balance"] = pa.return_account_balance(date_cursor=curr_date)
+    # Get invoices
+    invoices = Invoice.query.filter_by(policy_id=policy_id) \
+                      .filter(Invoice.bill_date <= curr_date ) \
+                      .filter(Invoice.deleted == False) \
+                      .order_by(Invoice.bill_date) \
+                      .all()
+
+    for invoice in invoices:
+        dict_invoice = {
+            'bill_date': invoice.bill_date.strftime("%m/%d/%Y"),
+            'due_date': invoice.due_date.strftime("%m/%d/%Y"),
+            'cancel_date': invoice.cancel_date.strftime("%m/%d/%Y"),
+            'amount_due': invoice.amount_due
+        }
+        list_invoices.append(dict_invoice)
+
+    response['invoices'] = list_invoices
+
+    return json.dumps(response)
+
+
